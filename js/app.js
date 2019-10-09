8473f7217c94337c7afa4b2e5660b2b353836d70
Tim Malstead <timmmalstead@gmail.com>
2019-10-09 11:10:49 -0700

    got the winning rounds working correctly

--- js/app.js
+++ js/app.js
@@ -16,11 +16,11 @@ level : 1,
 
 round : 1,
 
-lives : 3,
+lives : 10,
 
-gotItRight : true,
+openingModals : true,
 
-// dynamicInterval : 1000,
+readyToClick : false,
 
 setUpLevel() {
     if (this.level === 1) {
@@ -128,58 +128,36 @@ displayRandomPattern() {
 
         let flashNumber = Math.floor(Math.random() * numberOfTiles)
 
-        if (flashNumber === 0) {
-            flashNumber = 1
-        }
         for (let i = 0; i < flashNumber; i++){
 
             let random = Math.floor(Math.random() * numberOfTiles)
-            
-            if (random === 0) {
-                random = 1
-            }
 
-
-            // pattern.push(random)
-            // patternCheck.push(random)
-
-            if (pattern.length === 0 || pattern[i] !== random && pattern[i-1] !== random && pattern[i+1] !== random) {
-                pattern.push(random)
-                patternCheck.push(random)
-            }
-
+            pattern.push(random)
+            patternCheck.push(random)
         }
+
+        if (pattern.length === 0 && patternCheck.length === 0) {
+            const backupRandom = Math.floor(Math.random() * numberOfTiles)
+            pattern.push(backupRandom)
+            patternCheck.push(backupRandom)
+        }
+
         const patternInterval = setInterval(() => {
-            if (pattern.length === 0 && this.level === 1 && this.round === 1) {
+            if (pattern.length === 0 && this.openingModals === true) {
                 clearInterval(patternInterval)
-                for(i = 0; i < 100; i++) {
-                    window.clearInterval(i)
-                }
+                this.openingModals = false
                 this.secondModal()
                 }
 
-                // else if (pattern.length === 0) {
-                //     clearInterval(patternInterval)
-                //     for(i = 0; i < 100; i++) {
-                //         window.clearInterval(i)
-                //     }
-                //     console.log(patternCheck)
-                //     console.log("it's getting here")
-                //     console.log(patternInterval)
-                //     setTimeout(() => $("div").attr("class", "readyToClick"), 1000)
-                // }
-
                 else if (pattern.length === 0 && this.level >= 1 && this.round > 1) {
                     clearInterval(patternInterval)
-                    console.log("it's getting here")
-                    setTimeout(() => $("div").attr("class", "readyToClick"), 1000)
                 }
                 else {
                     if (pattern.length === 0) {
-                        pattern[0] = 1
+                        pattern[0] = 0
                     }
                 $("div").eq(pattern[0]).attr("class", "animated zoomIn")
-                setTimeout(() => $("div").removeClass(), 500)
+                setTimeout(() => $("div").removeClass(), 750)
                 pattern.shift(0)
             }
         },  1000)
@@ -209,36 +187,13 @@ secondModal() {
         }, 1000)
         $first.removeClass()
         $first.on("click", () => {
+            this.readyToClick = true
             $first.attr("class", "animated bounceOutUp")
             setTimeout(() => $first.css("display", "none"), 1000)
             setTimeout(() => $first.removeClass(), 1000)
             setTimeout(() => $first.attr("class", "modal"), 1000)
-            setTimeout(() => $("div").attr("class", "readyToClick"), 1000)
             })
     }
-        // else if (this.gotItRight === true && this.round > 1) {
-        //     const $right = $("#rightAnswer")
-        // setTimeout(() => {
-        //     $right.attr("class", "animated bounceInLeft")
-        //     $right.css("display", "block")
-        // }, 1000)
-        // $right.removeClass()
-        // $right.on("click", () => {
-        //     $right.attr("class", "animated bounceOutRight")
-        //     setTimeout(() => $right.css("display", "none"), 1000)
-        //     setTimeout(() => $right.removeClass(), 1000)
-        //     // setTimeout(() => $right.attr("class", "modal"), 1000)
-        //     // this.dynamicInterval += 500
-        //     // setTimeout(() => this.displayRandomPattern(), 2000)
-        //     this.displayRandomPattern()
-        //     })
-        // }
-        else if (this.gotItRight === false && this.lives !== 0) {
-            console.log("bruh, you got that one wrong")
-        }
-        else {
-            console.log("game over man!")
-        }
 },
 checkInput(flashToCheck) {
     if (flashToCheck === patternCheck[patternCheckCounter]) {
@@ -246,11 +201,9 @@ checkInput(flashToCheck) {
             if (patternCheckCounter === patternCheck.length) {
                 this.round++
                 $round.text(this.round)
-                this.gotItRight = true
                 setTimeout(() => $("div").removeClass(), 1000)
                 patternCheck = []
                 patternCheckCounter = 0
-                // this.secondModal()
                 this.displayRandomPattern()
             }
         }
@@ -259,8 +212,6 @@ checkInput(flashToCheck) {
         $lives.text(this.lives)
         this.round++
         $round.text(this.round)
-        this.gotItRight = false
-        // setTimeout(() => $("div").removeClass(), 1000)
         this.secondModal()
         }
     }
@@ -272,16 +223,14 @@ $("main").on("click", (e) => {
 
     const $arrayPosition = $(e.target).index()
 
-    if ($(e.target).hasClass("readyToClick") && e.target != e.currentTarget) {
+    if (game.readyToClick === true) {
         game.checkInput($arrayPosition)
     }
 })
 
-//almost completely certain that it's calling the display random function more than once as i select more and more. gonna have the mikes show me how to look for that in the debugger.
-
-  //have to figure out way to go through levels, rounds and deal with lives, add replay option etc
-  //add opening animation, see if you can't suss out shine effect from animate.css splash page
-  //add sounds?
-  //add backend for highscore?
-  //add social share?
-  //additional mechanics? I even wanna mess with that?
+//have to figure out way to go through levels, rounds and deal with lives, add replay option etc
+//add opening animation, see if you can't suss out shine effect from animate.css splash page
+//add sounds?
+//add backend for highscore?
+//add social share?
+//additional mechanics? I even wanna mess with that?
